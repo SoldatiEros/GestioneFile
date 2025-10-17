0b73ad7c99eb2312be3a036858842cb3ac1b19a5
Eros Soldati <EROS.SOLDATI@local.ittcesena.it>
2025-10-17 09:44:19 +0200

    Release 0.1: Creata la classe Writer

--- GestioneFile/Reader.cs
+++ GestioneFile/Reader.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace GestioneFile
 {
-    internal class Program
+    internal class Reader
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+
     }
 }
