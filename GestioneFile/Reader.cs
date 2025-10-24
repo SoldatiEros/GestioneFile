52cc16620b0e51b2ae5fd5f0907a33924a4f5f83
Eros Soldati <EROS.SOLDATI@local.ittcesena.it>
2025-10-24 09:37:42 +0200

    R0.1: Completata la classe reader

--- GestioneFile/Reader.cs
+++ GestioneFile/Reader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,30 @@ namespace GestioneFile
 {
     internal class Reader
     {
+        private string filePath;
+
+        public Reader(path)
+        {
+            filePath = Path;
+        }
+
+        public void LeggiFile()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write("Errore nella lettura del file" + ex.Message);
+            }
+        }
     }
 }
