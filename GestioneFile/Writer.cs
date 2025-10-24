e3eee732ed71dec8661220952c257cc39c442e13
Eros Soldati <EROS.SOLDATI@local.ittcesena.it>
2025-10-24 09:23:10 +0200

    R0.1: Completata la classe Writer

--- GestioneFile/Writer.cs
+++ GestioneFile/Writer.cs
@@ -9,5 +9,30 @@ namespace GestioneFile
 {
     internal class Writer
     {
+        private string filePath;
+
+        public Writer(string path)
+        {
+            filePath= path;
+        }
+
+        public void ScriviFile()
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine("Prima riga");
+                    writer.WriteLine("Seconda riga");
+                    writer.WriteLine("Terza riga");
+                }
+
+                Console.WriteLine("File scritto con successo");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Errore nella scrittura del file" + ex.Message);
+            }
+        }
     }
 }
